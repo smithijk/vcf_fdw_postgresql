513b09cab408864166474c4e80f4dd6d588349c6
Wenjian Yang <wenjian.yang@stjude.org>
2014-01-20 17:00:03 -0600

    refactor gtWideFdw, add vcfWrapper class as base class for other VCF wrappers

--- python/multicorn/vcffdw.py
+++ python/multicorn/vcffdw.py
@@ -3,6 +3,42 @@ import cyvcf
 from cyvcf import utils
 import glob, re
 
+class vcfWrapper (ForeignDataWrapper):
+  ''' parent class for other vcf wrappers '''
+
+  def __init__(self, options, columns):
+    super(vcfWrapper, self).__init__(options, columns)
+    self.columns = columns
+    self.readers = {}
+
+    directory = options.get('directory', None)
+    if directory is not None:
+      self.setup_vcf(directory)
+
+  def setup_vcf (self, directory):
+    self.readers = {} 
+    for vcf_file in glob.glob(directory):
+      if vcf_file is not None:
+        try:
+          self.readers[vcf_file] = cyvcf.Reader(filename=vcf_file)
+        except:
+          raise Exception("Can not open vcf file: " + vcf_file)
+    
+  def reset_samples (self):
+    for reader in self.readers.values():
+      reader.reset_samples()
+    return (self.samples())
+
+  def subset_by_samples (self, samples):
+    for reader in self.readers.values():
+      reader.subset_by_samples(samples)
+
+  def samples(self):
+    all_samples = set()
+    for reader in self.readers.values():
+      all_samples = all_samples.union(reader.samples)
+    return list(all_samples)
+
 class genotypeFdw (ForeignDataWrapper):
   def __init__(self, options, columns):
     super(genotypeFdw, self).__init__(options, columns)
@@ -65,30 +101,20 @@ class genotypeFdw (ForeignDataWrapper):
             line['genotype'] = record.genotype(s)['GT']
             yield line
 
-class gtWideFdw (ForeignDataWrapper):
+class gtWideFdw (vcfWrapper):
   ''' return vcf in a wide format
       only accept a single VCF
   '''
   def __init__(self, options, columns):
     super(gtWideFdw, self).__init__(options, columns)
-    self.columns = columns
-
-    self.vcf_file = options.get('vcf_file', None)
-    if self.vcf_file is None:
-      raise Exception("VCF file needs to be specified")
-    try:
-      self.reader = cyvcf.Reader(filename=self.vcf_file)
-    except:
-      raise Exception("Can not open vcf file: " + self.vcf_file)
 
   def execute(self, quals, columns):
-    self.reader.reset_samples()
-
     begin = None
     stop = None
     chrom = None
-    sample = None
+    qsample = None
     filter = None
+    directory = None
     for qual in quals:
       if qual.field_name == 'begin':
         begin = qual.value
@@ -97,45 +123,60 @@ class gtWideFdw (ForeignDataWrapper):
       elif qual.field_name == 'chrom':
         chrom = qual.value
       elif qual.field_name == 'sample':
-        sample = qual.value
+        qsample = qual.value
       elif qual.field_name == 'filter':
         filter = qual.value
+      elif qual.field_name == 'directory':
+        directory = qual.value
+
+    ''' allow update vcf files other than those used in options duringtable definition '''
+    if directory is not None:
+      self.setup_vcf(directory)
+
+    if len(self.readers) == 0:
+      raise Exception("No VCF file is opened.")
 
     if (chrom is None or begin is None or stop is None):
       raise Exception("chrom, begin and stop need to be specified")
 
-    ''' Not clear if below would gain anything '''
-    if (sample is None):
-      wanted_sample = self.reader.samples
-    elif (type(sample) != list):
-      wanted_sample = re.split('\|', sample)
+    ''' Select samples to be returned, not clear if it would improve speed in some situations '''
+    all_samples = self.reset_samples()
+
+    if (qsample is None):
+      wanted_sample = all_samples
+    elif (type(qsample) != list):
+      wanted_sample = re.split('\|', qsample)
     else:
       raise Exception("query 'sample in (a,b,c)' is not implemented. Use sample='a|b|c' instead.")
 
     wanted_sample = [s for s in wanted_sample if s in columns]
 
-    if (len(wanted_sample) != len(self.reader.samples)):
-      self.reader.subset_by_samples(wanted_sample)
+    if (len(wanted_sample) != len(all_samples)):
+      self.subset_by_samples(wanted_sample)
       
-    line = { 'file' : self.vcf_file,
+    line = { 'directory' : directory,
              'begin' : begin,
              'stop' : stop,
-             'sample' : sample,
+             'sample' : qsample,
     }
-    
-    for record in self.reader.fetch(chrom, begin, stop):
-      line['chrom'] = record.CHROM
-      line['pos'] = record.POS
-      line['id'] = record.ID
-      line['ref'] = record.REF
-      line['alt'] = record.ALT
-      line['qual'] = record.QUAL
-      line['filter'] = record.FILTER
-      line['format'] = record.FORMAT
-      line['info'] = record.INFO
-      for s in self.reader.samples:
-        line[s] = record.genotype(s)['GT']
-      yield line
+
+    for vcf_file, curr_reader in self.readers.items():
+      line['file'] = vcf_file
+      if len(wanted_sample) > 0 and len(curr_reader.samples) == 0:
+        continue
+      for record in curr_reader.fetch(chrom, begin, stop):
+        line['chrom'] = record.CHROM
+        line['pos'] = record.POS
+        line['id'] = record.ID
+        line['ref'] = record.REF
+        line['alt'] = record.ALT
+        line['qual'] = record.QUAL
+        line['filter'] = record.FILTER
+        line['format'] = record.FORMAT
+        line['info'] = record.INFO
+        for s in curr_reader.samples:
+          line[s] = record.genotype(s)['GT']
+        yield line
 
 class sampleFdw (ForeignDataWrapper):
   def __init__(self, options, columns):
